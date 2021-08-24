d46a98e19128a950dbdbb8ab924a529998f568bc
mhamzaq869 <mhamzaq869@gmail.com>
2021-08-23 21:58:42 -0700

    bokking detail

--- app/Http/Controllers/Tutor/BookingController.php
+++ app/Http/Controllers/Tutor/BookingController.php
@@ -4,15 +4,31 @@ namespace App\Http\Controllers\Tutor;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Booking;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 class BookingController extends Controller
 {
     /**
      *  Return Tutor Booking view
      */
 
-    public function index(){
-        return view('tutor.pages.booking.index');
+    public function index()
+    {
+        $today = Booking::where('booked_tutor',Auth::user()->id)->today()->get();
+        $tomorrow = Booking::where('booked_tutor',Auth::user()->id)->tomorrow()->get();
+        $pending = Booking::where('booked_tutor',Auth::user()->id)->status(0)->get();
+        $delivered = Booking::where('booked_tutor',Auth::user()->id)->status(1)->get();
+
+        return view('tutor.pages.booking.index',compact('today','tomorrow','pending','delivered'));
+    }
+
+    /**
+     *  Return Tutor Chat view
+     */
+    public function bookingDetail($id){
+        $booking = Booking::find($id);
+        return view('tutor.pages.booking.booking_detail',compact('booking'));
     }
 
     /**
