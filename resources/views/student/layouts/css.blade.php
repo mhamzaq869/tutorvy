5fba593ce4f79fcaec8ce86bb3355e8d6cbaf23d
MohsinMalik2 <malikmuhsin70@gmail.com>
2021-08-26 15:39:58 +0500

    student dashboard complete

--- resources/views/student/layouts/css.blade.php
+++ resources/views/student/layouts/css.blade.php
@@ -4,7 +4,11 @@
 <link href="{{ asset('assets/css/tutor-mobile.css') }}" rel="stylesheet">
 @else
 <link href="{{ asset('assets/css/student-home.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/home.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/booknow.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/tutor-asset.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/css/student-asset.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/css/student-mobile.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/tutor-mobile.css') }}" rel="stylesheet">
 
 @endif
