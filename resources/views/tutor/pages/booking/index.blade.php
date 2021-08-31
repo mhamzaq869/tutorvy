c5d1da2d6518217b2d1f6e6557b40ea4e5921c73
Muhammad Kashif <muhammadkashif70000@gmail.com>
2021-08-31 17:46:41 +0500

    booking changes

--- resources/views/tutor/pages/booking/index.blade.php
+++ resources/views/tutor/pages/booking/index.blade.php
@@ -16,89 +16,7 @@
                     </p>
                 </div>
             </div>
-            
-            <div class="row bg-white ml-2 mr-2">
-                <div class="col-md-12 mt-3">
-                    <nav class="">
-                        <div class="nav nav-stwich pb-0" id="nav-tab" role="tablist">
-                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab"
-                                href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">
-                                New Bookings
-                            </a>
-                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab"
-                                href="#nav-profile" role="tab" aria-controls="nav-profile"
-                                aria-selected="false">
-                                Upcomming
-                            </a>
-                            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab"
-                                href="#nav-contact" role="tab" aria-controls="nav-contact"
-                                aria-selected="false">
-                                Pending
-                            </a>
-                            <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about"
-                                role="tab" aria-controls="nav-about" aria-selected="false">
-                                Delivered
-                            </a>
-                        </div>
-                    </nav>
-                    <div class="tab-content" id="nav-tabContent">
-                        <div class="tab-pane tab-border-none tab-border-none-1 fade show active" id="nav-home" role="tabpanel"
-                            aria-labelledby="nav-home-tab">
-                                <div class="container-fluid ">
-                                    <div class="row">
-                                        <div class="col-md-12 col-sm-12 col-xs-12">
-                                            <table class="table table-bordered">
-                                                <thead>
-                                                    <tr
-                                                        style="font-family: Poppins;font-size: 14px;color: #00132D; border-top: 1px solid #D6DBE2;border-bottom: 1px solid #D6DBE2;">
-                                                        <th scope="col">
-                                                            Subjects
-                                                        </th>
-                                                        <th scope="col">
-                                                            Topic
-                                                        </th>
-                                                        <th scope="col">
-                                                            Time
-                                                        </th>
-                                                        <th scope="col">
-                                                            Student
-                                                        </th>
-                                                        <th scope="col">
-                                                            Duration
-                                                        </th>
-                                                        <th scope="col">
-                                                            Payment
-                                                        </th>
-                                                        <th scope="col"></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    @foreach ($today as $booking)
-                                                    <tr>
-                                                        <td class="pt-4">
-                                                        {{$booking->subject->name}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                        {{$booking->topic}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                        {{date("g:i a", strtotime("$booking->class_time UTC"))}} - {{$booking->class_date }}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            {{$booking->user->fullname}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            &nbsp;00:30:00
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            &nbsp;500$
-                                                        </td>
 
-<<<<<<< HEAD
-    <div class="container-fluid">
-        <p id="sidenav-toggles" class="heading-first  mr-3 mb-4 ml-2">
-            Bookings
-        </p>
         <div class="row bg-white ml-2 mr-2">
             <div class="col-md-12 mt-3">
                 <nav class="">
@@ -231,63 +149,6 @@
                                                     <td class="pt-4">
                                                         &nbsp;${{$booking->price}}
                                                     </td>
-=======
-                                                        <td style="text-align: center;">
-                                                            <a href="{{route('tutor.booking.detail',[$booking->id])}}">
-                                                                <button class="schedule-btn" type="button">
-                                                                    View details
-                                                                </button>
-                                                            </a>
-                                                        </td>
-                                                    </tr>
-                                                    @endforeach
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- end -->
-                        </div>
-                        <div class="tab-pane tab-border-none fade" id="nav-profile" role="tabpanel"
-                            aria-labelledby="nav-profile-tab">
-                                <div class="container-fluid ">
-                                    <div class="row">
-                                        <div class="col-md-12 col-sm-12 col-xs-12">
-                                            <table class="table table-bordered">
-                                                <thead>
-                                                    <tr
-                                                        style="font-family: Poppins;font-size: 14px;color: #00132D; border-top: 1px solid #D6DBE2;border-bottom: 1px solid #D6DBE2;">
-                                                        <th scope="col">Subjects</th>
-                                                        <th scope="col">Topic</th>
-                                                        <th scope="col">Time</th>
-                                                        <th scope="col">Student</th>
-                                                        <th scope="col">Duration</th>
-                                                        <th scope="col">Payment</th>
-                                                        <th scope="col"></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    @foreach ($tomorrow as $booking)
-                                                    <tr>
-                                                        <td class="pt-4">
-                                                        {{$booking->subject->name}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                        {{$booking->topic}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            {{date("g:i a", strtotime("$booking->class_time UTC"))}} - {{$booking->class_date }}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            {{$booking->user->fullname}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            &nbsp;00:30:00
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            &nbsp;500$
-                                                        </td>
->>>>>>> babb6dc6d714dcef6311ccbbb7b3eae9840d0401
 
                                                         <td style="text-align: center;">
                                                             <a href="{{route('tutor.booking.detail',[$booking->id])}}">
@@ -326,7 +187,6 @@
                                                 </thead>
                                                 <tbody>
 
-<<<<<<< HEAD
                                                 @foreach ($pending as $booking)
                                                 <tr>
                                                     <td class="pt-4">
@@ -347,28 +207,6 @@
                                                     <td class="pt-4">
                                                         &nbsp;${{$booking->price}}
                                                     </td>
-=======
-                                                    @foreach ($pending as $booking)
-                                                    <tr>
-                                                        <td class="pt-4">
-                                                        {{$booking->subject->name}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                        {{$booking->topic}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            {{date("g:i a", strtotime("$booking->class_time UTC"))}} - {{$booking->class_date }}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            {{$booking->user->fullname}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            &nbsp;00:30:00
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            &nbsp;500$
-                                                        </td>
->>>>>>> babb6dc6d714dcef6311ccbbb7b3eae9840d0401
 
                                                         <td style="text-align: center;">
                                                             <a href="{{route('tutor.booking.detail',[$booking->id])}}">
@@ -411,7 +249,6 @@
                                                 </thead>
                                                 <tbody>
 
-<<<<<<< HEAD
                                                 @foreach ($delivered as $booking)
                                                 <tr>
                                                     <td class="pt-4">
@@ -432,28 +269,6 @@
                                                     <td class="pt-4">
                                                         &nbsp;${{$booking->price}}
                                                     </td>
-=======
-                                                    @foreach ($delivered as $booking)
-                                                    <tr>
-                                                        <td class="pt-4">
-                                                        {{$booking->subject->name}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                        {{$booking->topic}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            {{date("g:i a", strtotime("$booking->class_time UTC"))}} - {{$booking->class_date }}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            {{$booking->user->fullname}}
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            &nbsp;00:30:00
-                                                        </td>
-                                                        <td class="pt-4">
-                                                            &nbsp;500$
-                                                        </td>
->>>>>>> babb6dc6d714dcef6311ccbbb7b3eae9840d0401
 
                                                         <td style="text-align: center;">
                                                             <a href="{{route('tutor.booking.detail',[$booking->id])}}">
