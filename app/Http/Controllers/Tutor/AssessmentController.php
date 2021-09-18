3d76c7642924d577127f84d37172da407693d164
MuzammilNaveed <muzamilnaveed10@gmail.com>
2021-09-18 19:24:59 +0500

    update

--- app/Http/Controllers/Tutor/AssessmentController.php
+++ app/Http/Controllers/Tutor/AssessmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Tutor;
 use App\Http\Controllers\Controller;
 use App\Models\Assessment;
 use Illuminate\Http\Request;
+use App\Models\subjectPlans;
 use Illuminate\Support\Facades\Auth;
 
 class AssessmentController extends Controller
@@ -28,6 +29,43 @@ class AssessmentController extends Controller
             'answer_2' => $request->answer_2,
             'answer_3' => $request->answer_3,
         ]);
+
+        $plans = [];
+
+        // $experty_level = array( 
+        //     "preElementary_rate" => 1,
+        //     "elementary" => 2,
+        //     "secondary" => 3,
+        //     "highSchool" => 4,
+        //     "postSec" => 5,
+        // );
+
+        if($request->preElementary == "on") {
+            array_push($plans , array("experty_level" => 1 , "rate" => $request->preElementary_rate));
+        }
+        if($request->elementary == "on") {
+            array_push($plans , array("experty_level" => 2 , "rate" => $request->elementary_rate));
+        }
+        if($request->secondary == "on") {
+            array_push($plans , array("experty_level" => 3 , "rate" => $request->secondary_rate));
+        }
+        if($request->highSchool == "on") {
+            array_push($plans , array("experty_level" => 4 , "rate" => $request->highSchool_rate));
+        }
+        if($request->postSec == "on") {
+            array_push($plans , array("experty_level" => 5 , "rate" => $request->postSec_rate));
+        }
+
+        for($i = 0; $i < count($plans); $i++) {
+
+            subjectPlans::updateOrCreate([
+                "subject_id" => $request->subject,
+                "user_id" => Auth::user()->id,
+                "experty_level" => $plans[$i]['experty_level'],
+                "rate" => $plans[$i]['rate'],
+            ]);
+
+        }
 
         return redirect()->route('tutor.dashboard');
     }
---
+++ app/Models/subjectPlans.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class subjectPlans extends Model
+{
+
+    protected $table = 'subject_plans';
+    protected $fillable = [
+        'user_id',
+        'subject_id',
+        'experty_level',
+        'rate'
+    ];
+
+    use HasFactory;
+}
