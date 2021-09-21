3ca1f197dc6e2938e1142fd0a55210dbbbeb0176
Muhammad Kashif <muhammadkashif70000@gmail.com>
2021-09-21 18:21:39 +0500

    dashboard changes

--- resources/views/js_files/student/tutorJs.blade.php
+++ resources/views/js_files/student/tutorJs.blade.php
@@ -2,15 +2,6 @@
 
 let tutors = '';
 
-(function () {
-    var user_language_code = "{{ $user->language ?? 'en-US'}}";
-    var option = '<option value=""> Select Language</option>';
-    // for (var language_code in languages_list) {
-    //     var selected = (language_code == user_language_code) ? ' selected' : '';
-        option += '<option value="' + language_code + '">' + languages_list[language_code] + '</option>';
-    }
-    document.getElementById('languages-list').innerHTML = option;
-})();
 
 
 $('#subjects-list').on("change", function(e) {
