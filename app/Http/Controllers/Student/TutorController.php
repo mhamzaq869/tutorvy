1f0836f5e03675b6e35dd9a73a06203bbc61e75f
Muhammad Kashif <muhammadkashif70000@gmail.com>
2021-09-21 18:05:24 +0500

    dashboard changes

--- app/Http/Controllers/Student/TutorController.php
+++ app/Http/Controllers/Student/TutorController.php
@@ -18,16 +18,24 @@ class TutorController extends Controller
 
     public function index()
     {
-             
-        $available_tutors = DB::table('users')
+        $subject = \Auth::user()->std_learn;
+          
+        $query = DB::table('users')
         ->select('view_tutors_data.*','teachs.subject_id as subject_id')
         ->leftJoin('teachs', 'users.id', '=', 'teachs.user_id')
         ->leftJoin('view_tutors_data', 'view_tutors_data.id', '=', 'users.id')
-        ->where('teachs.subject_id', \Auth::user()->std_learn )
         ->where('users.role',2)
-        ->where('users.status',2)
-        ->get();
+        ->where('users.status',2);
 
+        $query->where(function($query2) use ($subject)
+        {
+            if($subject != null && $subject != ''){
+                $query2->where('teachs.subject_id', $subject);
+            }
+            
+        });
+
+        $available_tutors = $query->get();
 
         foreach($available_tutors as $tutor) {
             $tutor->is_favourite = DB::table("fav_tutors")->where("user_id",Auth::user()->id)->where("tutor_id",$tutor->id)->first();
