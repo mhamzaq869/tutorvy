308a8788ff1b04e8551916f2a4bfbcc795428f65
MuzammilNaveed <muzamilnaveed10@gmail.com>
2021-09-26 17:12:51 +0500

    update

--- public/assets/firebase/index.js
+++ public/assets/firebase/index.js
@@ -1,84 +1,26 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
-import { getMessaging, getToken, onMessage  } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging.js';
-
 const firebaseConfig = {
-  apiKey: "AIzaSyANnRCERSRDsQKB1tTSbPsV0y7JkAbod-0",
-  authDomain: "todaylecture.firebaseapp.com",
-  databaseURL: "https://todaylecture.firebaseio.com",
-  projectId: "todaylecture",
-  storageBucket: "todaylecture.appspot.com",
-  messagingSenderId: "229770008752",
-  appId: "1:229770008752:web:2df73a2333da1d73b4c375",
-  measurementId: "G-89WMDYHMG6"
+  apiKey: "AIzaSyCIoIw5TgIOYXirhqBlsYsYJOMMNStK_KA",
+  authDomain: "tutorvy-ad64f.firebaseapp.com",
+  projectId: "tutorvy-ad64f",
+  storageBucket: "tutorvy-ad64f.appspot.com",
+  messagingSenderId: "30326081925",
+  appId: "1:30326081925:web:342e89a81d7d7f396ddcec",
+  measurementId: "G-DXS0PNV01R"
 };
-
-const app = initializeApp(firebaseConfig);
-
-// Get registration token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-const messaging = getMessaging();
-getToken(messaging, { vapidKey: 'BLVqHTURHbHlM3aUGrU5qhW6GwwWsES9n_4An4W9aRTeW67sJPOg6SGRmZ7EvXVIXQMVqRLwHazcpIRb9Qg5x-Y' }).then((currentToken) => {
-  if (currentToken) {
-    // Send the token to your server and update the UI if necessary
-    // ...
-    var check_value = $(".token_wrapper").data('id');
-    if(check_value == 0) {
-      saveFcmToken(currentToken);
-    } 
-    console.log(currentToken , "currentToken");
-  } else {
-    // Show permission request UI
-    console.log('No registration token available. Request permission to generate one.');
-    // ...
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  // ...
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+// firebase chat db object
+var db = firebase.firestore();
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey("BCUzgeYCI95gituGxynAXXezgC3kt8LobvtNRB0PwO-0iPdFYQKeMAqHJq0R-JhnxT2OVocWCgKIDTgZFaAIIu8");
+// messaging.useServiceWorker('/framework/firebase-messaging-sw.js')
+messaging.requestPermission().then(function(){
+  return messaging.getToken();
+}).then(function(token){
+  console.log(token)
+}).catch(function(err){
+  console.log('unable to get permission.'+err)
 });
-
-const messagings = getMessaging();
-onMessage(messagings, (payload) => {
+messaging.onMessage((payload) => {
   console.log('Message received. ', payload);
-  // ...
 });
-
-// firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
-
-
-// const messaging = firebase.messaging();
-// messaging.usePublicVapidKey("BK8CHpqRSc6qjQDVSc6eQZCOganeqlTqBZa4kpAWOIHhYJFQPdNqe8rcngMWITo1UkHTcq9AQBva7Zuf8GRY0Hw");
-// // messaging.useServiceWorker('/framework/firebase-messaging-sw.js')
-        
-// messaging.requestPermission().then(function(){
-//   return messaging.getToken();
-// }).then(function(token){
-//   $('#fcm_token').val(token)
-//   console.log(token)
-// }).catch(function(err){
-//   console.log('unable to get permission.'+err)
-// });
-
-// messaging.onMessage((payload) => {
-//   console.log('Message received. ', payload);
-//   // ...
-// });
-function saveFcmToken(token) {
-  var origin = window.location.origin;
-
-  $.ajax({
-      headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      },
-      url: origin + "/general/save-token",
-      type:"POST",
-      data:{token:token},
-      dataType: 'json',
-      success:function(response){
-        console.log(response , "token")
-      },
-      error:function(e) {
-          console.log(e)
-      }
-  });
-}
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,53 +1,34 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging.js');
-// importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging/sw.js');
-
+importScripts('https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyANnRCERSRDsQKB1tTSbPsV0y7JkAbod-0",
-  authDomain: "todaylecture.firebaseapp.com",
-  databaseURL: "https://todaylecture.firebaseio.com",
-  projectId: "todaylecture",
-  storageBucket: "todaylecture.appspot.com",
-  messagingSenderId: "229770008752",
-  appId: "1:229770008752:web:2df73a2333da1d73b4c375",
-  measurementId: "G-89WMDYHMG6"
+    apiKey: "AIzaSyCIoIw5TgIOYXirhqBlsYsYJOMMNStK_KA",
+    authDomain: "tutorvy-ad64f.firebaseapp.com",
+    projectId: "tutorvy-ad64f",
+    storageBucket: "tutorvy-ad64f.appspot.com",
+    messagingSenderId: "30326081925",
+    appId: "1:30326081925:web:342e89a81d7d7f396ddcec",
+    measurementId: "G-DXS0PNV01R"
 };
-
-const messaging = getMessaging();
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
-
-// const app = initializeApp(firebaseConfig);
-// // firebase.analytics();
-// // Retrieve an instance of Firebase Messaging so that it can handle background
-// // messages.
-// const messaging = firebase.messaging();
-
-// messaging.onBackgroundMessage((payload) => {
-//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//     // Customize notification here
-//     const notificationTitle = 'Background Message Title';
-//     const notificationOptions = {
-//       body: 'Background Message body.',
-//       icon: '/firebase-logo.png'
-//     };
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
   
-//     // self.registration.showNotification(notificationTitle,
-//     //   notificationOptions);
-//   });
+    // self.registration.showNotification(notificationTitle,
+    //   notificationOptions);
+  });
