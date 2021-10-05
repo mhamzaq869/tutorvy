1d389c5827e788d23452e6d56d2a09c4b11b6b5a
MuzammilNaveed <muzamilnaveed10@gmail.com>
2021-10-05 18:10:15 +0500

    update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -29,6 +29,12 @@
     <link rel="stylesheet" href="{{ asset('assets/css/home.css')}}" />
     <link rel="stylesheet" href="{{ asset('assets/css/dropify.css')}}" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <style>
+        .bgm{
+            text-decoration:none !important;
+            color:black !important;
+        }
+    </style>
 
 </head>
 <!-- <body>
