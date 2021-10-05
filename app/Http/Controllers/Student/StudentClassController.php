f96a91de600a2ad9f02db15d3181458875c69f73
MuzammilNaveed <muzamilnaveed10@gmail.com>
2021-10-05 19:02:09 +0500

    update

--- app/Http/Controllers/Student/StudentClassController.php
+++ app/Http/Controllers/Student/StudentClassController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Classroom;
 use App\Models\Activitylogs;
+use App\Models\ClassroomLogs;
 use App\Models\Booking;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
